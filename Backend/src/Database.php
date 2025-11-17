c501e30d720e11d94d0e9a3e89d67437c1598637
myhplaptop <tertexpert@gmail.com>
2025-11-17 11:32:29 +0800

    added linux directory

--- Backend/src/Database.php
+++ Backend/src/Database.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 class Database {
 
   private \PDO $pdo;
-
+  // directoy for linux - sqlite:/opt/lampp/htdocs/batcave/Batcave/Backend/src/mydatabase.db
   public function __construct(private string $path = 'D:\BASTITE\xampp2\htdocs\BatCave\Backend\database\batcave.db'){
     $this->connect();
   }
